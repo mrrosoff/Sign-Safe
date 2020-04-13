6079ea1e2edecd606559468e95012f07313299b4
mrosoff <mrosoff@ramscolostate.edu>
2020-04-13 13:08:19 -0600

    Test Lambda Try 14

--- Lambda/mongo.js
+++ Lambda/mongo.js
@@ -11,7 +11,7 @@ const getData = async (data) => {
 		return await client
 		.db('sample_weatherdata')
 		.collection('data')
-		.find({}).maxTimeMS(1000).toArray();
+		.find({}).maxTimeMS(1000).max(10).toArray();
 	}
 
 	catch (err)
