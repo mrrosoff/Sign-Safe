1b170692dc725fbc333ef5eb22f7614ebe958b7b
mrosoff <mrosoff@ramscolostate.edu>
2020-04-13 13:58:15 -0600

    Test if this works?

---
+++ netlify-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export NVM_DIR="/home/mrosoff/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+npm run dev
