69db71354b975a1c15410a083aef604a51db3d6d
mrosoff <mrosoff@ramscolostate.edu>
2020-04-13 16:10:22 -0600

    Add Unique URL Routing, URL State.

--- Client/Components/Pages/ContractPage.js
+++ Client/Components/Pages/ContractPage.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 
 import { Container, Grid, Typography } from "@material-ui/core";
 import {callLambdaFunction} from "../../Hooks/restfulAPI";
+import {PrimaryButton} from "../Elements/Buttons";
 
 const ContractPage = props =>
 {
@@ -31,10 +32,25 @@ const ContractPage = props =>
 						This URL ({contractURL}) is at status: {urlStatus}
 					</Typography>
 				</Grid>
+				<Grid item>
+					<PrimaryButton
+						text={"Update URL State"}
+						onClick={() => updateContractState(contractURL, urlStatus + 1, setURLStatus)}
+					/>
+				</Grid>
 			</Grid>
 		</Container>
 
 	);
+};
+
+const updateContractState = (url, newState, setState) =>
+{
+	callLambdaFunction("updateURLStatus", {
+		url: url, urlState: newState
+	}).then(r => console.log(r));
+
+	setState(newState);
 };
 
 export default ContractPage;
