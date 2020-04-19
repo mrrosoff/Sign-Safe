a8784d9f15739dda96021caeca339ce525a95758
mrosoff <mrosoff@ramscolostate.edu>
2020-04-18 21:48:49 -0600

    Add Conditional Render for Contract Owner Page

--- Client/Components/Pages/ContractPages/ContractPageCreator.js
+++ Client/Components/Pages/ContractPages/ContractPageCreator.js
@@ -10,7 +10,28 @@ const useStyles = makeStyles((theme) => ({ root: { width: '100%' } }));
 const ContractPageCreator = props =>
 {
 	let [contractUploaded, setContractUploaded] = useState(false);
-	let [image, setImage] = useState();
+
+	let view;
+
+	if(props.urlStatus === 0)
+	{
+		view = <UploadContractView contractUploaded={contractUploaded} setContractUploaded={setContractUploaded} {...props} />;
+	}
+
+	else if(props.urlStatus === 1)
+	{
+		view = <AddSignersView contractUploaded={contractUploaded} {...props}/>;
+	}
+
+	else if(props.urlStatus === 2)
+	{
+		view = <CloseContractView contractUploaded={contractUploaded} {...props} />;
+	}
+
+	else
+	{
+		view = null;
+	}
 
 	return(
 		<Grid
@@ -23,30 +44,71 @@ const ContractPageCreator = props =>
 			spacing={4}
 		>
 			<Grid item xs={9}>
-				<Grid
-					container
-					justify={"center"}
-					alignItems={"center"}
-					alignContent={"center"}
-					style={{height: "100%"}}
-					spacing={2}
-				>
-					<Grid item xs={6}>
-						<UploadButton text={"Upload Contract"} accept={".png, .csv, .jpg"} onClick={(e) => {
-							setImage(URL.createObjectURL(e.target.files[0]));
-							setContractUploaded(true);
-						}}/>
-					</Grid>
-					<Grid item xs={6} style={{width: "100%"}}>
-						<img width={"100%"} height={"auto"} src={image} alt={"temp"}/>
-					</Grid>
-				</Grid>
+				{view}
 			</Grid>
 			<Grid item style={{width: "100%"}}>
 				<CreatorStepper contractUploaded={contractUploaded} {...props}/>
 			</Grid>
 		</Grid>
 	)
+};
+
+const UploadContractView = props =>
+{
+	let [image, setImage] = useState();
+
+	return(
+		<Grid
+			container
+			justify={"center"}
+			alignItems={"center"}
+			alignContent={"center"}
+			style={{height: "100%"}}
+			spacing={2}
+		>
+			<Grid item xs={6}>
+				<UploadButton text={"Upload Contract"} accept={".png, .csv, .jpg"} onClick={(e) => {
+					setImage(URL.createObjectURL(e.target.files[0]));
+					props.setContractUploaded(true);
+				}}/>
+			</Grid>
+			<Grid item xs={6} style={{width: "100%"}}>
+				<img width={"100%"} height={"auto"} src={image} alt={"temp"}/>
+			</Grid>
+		</Grid>
+	);
+};
+
+const AddSignersView = props =>
+{
+	return(
+		<Grid
+			container
+			justify={"center"}
+			alignItems={"center"}
+			alignContent={"center"}
+			style={{height: "100%"}}
+			spacing={2}
+		>
+
+		</Grid>
+	);
+};
+
+const CloseContractView = props =>
+{
+	return(
+		<Grid
+			container
+			justify={"center"}
+			alignItems={"center"}
+			alignContent={"center"}
+			style={{height: "100%"}}
+			spacing={2}
+		>
+
+		</Grid>
+	);
 };
 
 const CreatorStepper = props =>
