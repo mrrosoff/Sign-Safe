1133dd1feba26ed64e097f76bf2172296f9d345d
mrosoff <mrosoff@ramscolostate.edu>
2020-04-18 20:51:09 -0600

    Small Refactors, add scss support...

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,10 @@ module.exports = {
 	devServer: { port: 3000, open: false, hot: true, historyApiFallback: true },
 	module: {
 		rules: [
-			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", options: { presets: ["@babel/preset-env", "@babel/preset-react"]}},
-			{test: /\.css$/, loader: ['style-loader', 'CSS-loader',]},
-			{test: /\.(png|svg|jpg|gif)$/, loader: ['file-loader',]},
+			{test: /\.js$/, exclude: /node_modules/, use: "babel-loader", options: { presets: ["@babel/preset-env", "@babel/preset-react"]}},
+			{test: /\.css$/, use: ['style-loader', 'css-loader',]},
+			{test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader']},
+			{test: /\.(png|svg|jpg|gif)$/, use: ['file-loader',]},
 		]
 	},
 	plugins: [
