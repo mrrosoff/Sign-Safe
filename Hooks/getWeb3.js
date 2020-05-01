9764c1c368bb955556880377d36a3495fb4a2ea2
Max <maxrosoff1@gmail.com>
2020-04-30 18:42:07 -0600

    Wallets

--- Hooks/getWeb3.js
+++ Hooks/getWeb3.js
@@ -23,6 +23,14 @@ export const getWeb3 = async (setEthAccount) =>
 {
     let web3 = null;
 
+    const wallets = [
+        { walletName: "coinbase", preferred: true },
+        { walletName: "metamask", preferred: true },
+        { walletName: "torus", preferred: true },
+        { walletName: "dapper",  },
+        { walletName: "unilogin" },
+    ];
+
     const onboard = Onboard(
         {
             dappId: "d8557b0d-3b65-4826-b336-d502f90f1b6f",
@@ -31,7 +39,8 @@ export const getWeb3 = async (setEthAccount) =>
                 {
                     address: address => setEthAccount(address.toLowerCase()),
                     wallet: wallet => web3 = new Web3(wallet.provider)
-                }
+                },
+            walletSelect: { wallets: wallets }
         }
     );
 
